c7ea9ad3e1e394c0114d95fccf998f959c73f0cb
Vlad-Alexandru Romila <romilavlad2001@gmail.com>
2026-03-28 11:25:14 +0200

    fixes

--- terraform/deaddrop_project.tf
+++ terraform/deaddrop_project.tf
@@ -3,7 +3,7 @@ module "deaddrop_frontend" {
   source              = "./modules/cloudfront-spa"
   project_name        = "${local.resource_identifier}-deaddrop-fe"
   domain_name         = "deaddrop.${local.resource_identifier}.fiipractic.com"
-  hosted_zone_id      = data.aws_route53_zone.main_hosted_zone.id
+  hosted_zone_id      = aws_route53_zone.main_hosted_zone.id
   project_description = "Dead Drop — self-destructing secret sharing"
   build_command       = "npm ci; npm run build"
   build_working_dir   = "${path.module}/../deaddrop/fe"
--- terraform/route53.tf
+++
@@ -1,3 +0,0 @@
-data "aws_route53_zone" "main_hosted_zone" {
-  name = "${local.resource_identifier}.fiipractic.com"
-}
